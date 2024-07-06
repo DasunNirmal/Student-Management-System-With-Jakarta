98a480178fa4e73ebd869686ae42096902b717d3
DasunNirmal <dasun200301@gmail.com>
2024-07-06 10:36:49 +0530

    Updated lombok added

--- src/main/java/lk/ijse/studentmanagementsystemwithjee/dto/Student.java
+++ src/main/java/lk/ijse/studentmanagementsystemwithjee/dto/Student.java
@@ -1,7 +1,14 @@
 package lk.ijse.studentmanagementsystemwithjee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Student implements Serializable {
     private String id;
     private String name;
