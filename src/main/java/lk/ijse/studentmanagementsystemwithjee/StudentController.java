5ec381dbc343a456e41410581b3e1785dc2ddf78
DasunNirmal <dasun200301@gmail.com>
2024-07-06 08:49:22 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/studentmanagementsystemwithjee/StudentController.java
@@ -0,0 +1,56 @@
+package lk.ijse.studentmanagementsystemwithjee;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/Student")
+public class StudentController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*Todo:save student*/
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*Todo:*/
+        if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
+            /*Send Error*/
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+
+        /*Process*/
+        /*BufferedReader reader = req.getReader();
+        StringBuilder sb = new StringBuilder();
+        PrintWriter writer = resp.getWriter();
+        reader.lines().forEach(line -> sb.append(line).append(line + "\n"));
+        System.out.println(sb);
+        writer.write(sb.toString());
+        writer.close();*/
+
+        /*JSON Manipulation with Parson*/
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        System.out.println(jsonObject.getString("email"));
+    }
+
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*Todo:Delete Student*/
+    }
+
+}
